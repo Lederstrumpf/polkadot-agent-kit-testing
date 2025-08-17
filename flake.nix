c654db2181836842fa56ea5a99f861967c9405f1
Robert Hambrock <roberthambrock@gmail.com>
2025-08-17 20:03:27 +0200

    add basic flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Nix flake for polkadot-agent-kit monorepo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            nodePackages.pnpm
+            typescript
+            ollama
+          ];
+        };
+      }
+    );
+}
